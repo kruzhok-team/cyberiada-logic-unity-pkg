52d454cc005d16ada904228f4b4fe3931b480fbc
Dmitry Nesterov <d.nesterov@jetstyle.ru>
2024-10-15 19:39:42 +0500

    Switch recursion to loop for simplicity

--- Runtime/CyberiadaGraph/VerboseGraphComparator.cs
+++ Runtime/CyberiadaGraph/VerboseGraphComparator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Talent.Graphs
@@ -15,57 +16,71 @@ namespace Talent.Graphs
 
         private string ConvertToString(CyberiadaGraph graph)
         {
-            _stringBuilder.Clear();
-            _stringBuilder.AppendLine($"GRAPH({graph.ID})\n");
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"GRAPH({graph.ID})");
+            Dictionary<Node, CyberiadaGraph> graphByNode = graph.Nodes.ToDictionary(node => node, _ => graph);
+            LinkedList<Node> nodes = new LinkedList<Node>(graph.Nodes);
 
-            foreach (Node node in graph.Nodes)
+            while (nodes.Count > 0)
             {
-                AddNode(node);
+                LinkedListNode<Node> item = nodes.First;
+                stringBuilder.Append($"NODE({item.Value.ID})({item.Value.Data.Vertex}(");
+                _stringBuilder.AppendLine($"NodeData({item.Value.Data.VisualData.Name})({item.Value.Data.VisualData.Position})");
+
+                foreach (KeyValuePair<string, Event> @event in item.Value.Data.Events)
+                {
+                    stringBuilder.AppendLine($"{@event}\n");
+                }
+
+                stringBuilder.AppendLine(")");
+                if (item.Value.ParentNode != null)
+                {
+                    stringBuilder.AppendLine($"{nameof(item.Value.ParentNode)}={item.Value.ParentNode.ID}");
+                }
+
+                if (item.Value.NestedGraph != null)
+                {
+                    CyberiadaGraph innerGraph = item.Value.NestedGraph;
+                    stringBuilder.AppendLine($"GRAPH({innerGraph.ID})");
+                    LinkedListNode<Node> previousItem = item;
+                    foreach (var node in innerGraph.Nodes)
+                    {
+                        var nextItem = nodes.AddAfter(previousItem, node);
+                        graphByNode.Add(node, innerGraph);
+                        previousItem = nextItem;
+                    }
+                }
+
+                CyberiadaGraph currentGraph = graphByNode[item.Value];
+
+                if (currentGraph.Nodes.Last() == item.Value)
+                {
+                    foreach (Edge edge in currentGraph.Edges)
+                    {
+                        AddEdge(edge, stringBuilder);
+                    }
+                }
+
+                graphByNode.Remove(item.Value);
+                nodes.RemoveFirst();
             }
 
-            foreach (Edge edge in graph.Edges)
-            {
-                AddEdge(edge);
-            }
-
-            return _stringBuilder.ToString();
+            return stringBuilder.ToString();
         }
-        
-        private void AddNode(Node node)
+
+        private void AddEdge(Edge edge, StringBuilder stringBuilder)
         {
-            _stringBuilder.AppendLine($"NODE({node.ID})(");
-            _stringBuilder.AppendLine($"NodeData({node.Data.VisualData.Name})({node.Data.VisualData.Position})");
+            stringBuilder.AppendLine("EDGE");
+            stringBuilder.AppendLine($"{nameof(edge.SourceNode)}={edge.SourceNode}");
+            stringBuilder.AppendLine($"{nameof(edge.TargetNode)}={edge.TargetNode}");
+            stringBuilder.AppendLine($"EdgeData({edge.Data.TriggerID})({edge.Data.Condition})({edge.Data.VisualData.Position})");
 
-            foreach (KeyValuePair<string, Event> @event in node.Data.Events)
-            {
-                _stringBuilder.AppendLine($"{@event}\n");
-            }
-            
-            _stringBuilder.Append(")");
-            _stringBuilder.AppendLine($"{nameof(node.ParentNode)}={node.ParentNode?.ID}");
-
-            if (node.NestedGraph != null)
-            {
-                _stringBuilder.AppendLine(ConvertToString(node.NestedGraph));
-            }
-            
-            _stringBuilder.Append('\n');
-        }
-
-        private void AddEdge(Edge edge)
-        {
-            _stringBuilder.AppendLine("EDGE");
-            _stringBuilder.AppendLine($"{nameof(edge.SourceNode)}={edge.SourceNode}");
-            _stringBuilder.AppendLine($"{nameof(edge.TargetNode)}={edge.TargetNode}");
-            _stringBuilder.AppendLine($"{nameof(edge.Data)}=");
-            _stringBuilder.AppendLine($"EdgeData({edge.Data.TriggerID})({edge.Data.Condition})({edge.Data.VisualData.Position})");
-            
             foreach (Action action in edge.Data.Actions)
             {
-                _stringBuilder.Append($"\n{action}");
+                stringBuilder.AppendLine($"{action}");
             }
-            
-            _stringBuilder.Append('\n');
+
+            stringBuilder.Append('\n');
         }
     }
 }
