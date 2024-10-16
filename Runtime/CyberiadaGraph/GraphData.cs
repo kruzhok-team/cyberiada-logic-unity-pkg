4eb616ff4efd712daa4c1be9c52010396013fd88
git-ethan-mars <dima.nesterov.2002@inbox.ru>
2024-10-16 09:16:15 +0500

    Add meta property in GraphData

--- Runtime/CyberiadaGraph/GraphData.cs
+++ Runtime/CyberiadaGraph/GraphData.cs
@@ -1,9 +1,54 @@
+using System;
+using System.Collections.Generic;
+
 namespace Talent.Graphs
 {
     public class GraphData : IClonable<GraphData>
     {
         public string Name { get; set; }
         public string ReferenceGraphID { get; set; }
+
+        private Dictionary<string, string> DocumentMetaData { get; set; } = new Dictionary<string, string>();
+
+        public void AddMetaData(string key, string value)
+        {
+            if (key is null)
+            {
+                throw new NullReferenceException(nameof(key));
+            }
+
+            if (value is null)
+            {
+                throw new NullReferenceException(nameof(value));
+            }
+            
+            DocumentMetaData[key] = value;
+        }
+
+        public bool TryGetMetadata(string key, out string value)
+        {
+            if (key is null)
+            {
+                throw new NullReferenceException(nameof(key));
+            }
+            
+            return DocumentMetaData.TryGetValue(key, out value);
+        }
+
+        public void AddMetadata(string key, string value)
+        {
+            if (key is null)
+            {
+                throw new NullReferenceException(nameof(key));
+            }
+
+            if (value is null)
+            {
+                throw new NullReferenceException(nameof(value));
+            }
+            
+            DocumentMetaData.Add(key, value);
+        }
 
         /// <summary>
         /// Creates a copy of the graph data
